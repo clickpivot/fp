c1e87adbe02d0fa67476c2f5665d84ca9845433e
clickpivot <121286619+clickpivot@users.noreply.github.com>
2026-01-12 12:58:37 -0500

    Fix route definition for home page

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,9 @@ use App\Http\Controllers\Admin\PoolController as AdminPoolController;
 use App\Http\Controllers\Admin\FightController as AdminFightController;
 use App\Http\Controllers\Admin\ResultController as AdminResultController;
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
