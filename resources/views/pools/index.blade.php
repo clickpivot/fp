6e02f4669a7c6677bf6f18a5ceb510f750ca9bbe
clickpivot <121286619+clickpivot@users.noreply.github.com>
2026-01-12 13:04:09 -0500

    Handle null event date with fallback message

--- resources/views/pools/index.blade.php
+++ resources/views/pools/index.blade.php
@@ -62,7 +62,7 @@
                                         <svg class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
                                         </svg>
-                                        {{ $pool->event->event_date->format('M j, Y') }}
+                                        {{ $pool->event->event_date ? $pool->event->event_date->format('M j, Y') : 'Date TBA' }}
                                     </span>
                                     <span class="flex items-center">
                                         <svg class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -109,7 +109,7 @@
                                         <svg class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
                                         </svg>
-                                        {{ $pool->event->event_date->format('M j, Y') }}
+                                        {{ $pool->event->event_date ? $pool->event->event_date->format('M j, Y') : 'Date TBA' }}
                                     </span>
                                     <span class="flex items-center">
                                         <svg class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
